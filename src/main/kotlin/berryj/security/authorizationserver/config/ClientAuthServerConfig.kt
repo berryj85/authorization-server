eb87a3d5278f3c9f9d284f9dfe194298edfa49e9
Janepop Atirattanachai <jane3030@gmail.com>
2023-07-02 10:29:28 +0700

    ignore add role to jwt

--- src/main/kotlin/berryj/security/authorizationserver/config/ClientAuthServerConfig.kt
+++ src/main/kotlin/berryj/security/authorizationserver/config/ClientAuthServerConfig.kt
@@ -2,12 +2,17 @@ package berryj.security.authorizationserver.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.oauth2.core.AuthorizationGrantType
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod
 import org.springframework.security.oauth2.core.oidc.OidcScopes
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import java.util.*
 
 @Configuration
@@ -31,5 +36,24 @@ class ClientAuthServerConfig {
             .scope("read")
             .build();
         return InMemoryRegisteredClientRepository(registeredClient)
+    }
+
+    @Bean
+    fun clientSettings(): ClientSettings {
+        return ClientSettings.builder()
+            .requireAuthorizationConsent(false)
+            .requireProofKey(false)
+            .build()
+    }
+
+    @Bean
+    // 	An instance of UserDetailsService for retrieving users to authenticate.
+    fun userDetailsService(): UserDetailsService {
+        val encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder()
+        val userDetails = User.withUsername("admin")
+            .password(encoder.encode("password"))
+            .roles("USER")
+            .build()
+        return InMemoryUserDetailsManager(userDetails)
     }
 }
---
+++ src/main/kotlin/berryj/security/authorizationserver/config/TokenConfig.kt
@@ -0,0 +1,68 @@
+package berryj.security.authorizationserver.config
+
+import com.nimbusds.jose.jwk.JWKSet
+import com.nimbusds.jose.jwk.RSAKey
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet
+import com.nimbusds.jose.jwk.source.JWKSource
+import com.nimbusds.jose.proc.SecurityContext
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer
+import java.security.KeyPair
+import java.security.KeyPairGenerator
+import java.security.interfaces.RSAPrivateKey
+import java.security.interfaces.RSAPublicKey
+import java.util.*
+
+
+@Configuration
+class TokenConfig {
+    @Bean
+    fun jwkSource(): JWKSource<SecurityContext> {
+        val keyPair = generateRsaKey()
+        val publicKey = keyPair.public as RSAPublicKey
+        val privateKey = keyPair.private as RSAPrivateKey
+        val rsaKey: RSAKey = RSAKey.Builder(publicKey)
+            .privateKey(privateKey)
+            .keyID(UUID.randomUUID().toString())
+            .build()
+        val jwkSet = JWKSet(rsaKey)
+        return ImmutableJWKSet(jwkSet)
+    }
+
+    @Bean
+    // An instance of JwtDecoder for decoding signed access tokens.
+    fun jwtDecoder(jwkSource: JWKSource<SecurityContext>): JwtDecoder {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource)
+    }
+
+    @Bean
+    fun jwtCustomizer(): OAuth2TokenCustomizer<JwtEncodingContext> {
+        return OAuth2TokenCustomizer<JwtEncodingContext> { context: JwtEncodingContext ->
+            if (context.tokenType === OAuth2TokenType.ACCESS_TOKEN) {
+                val principal: Authentication = context.getPrincipal()
+                val authorities: List<String> = principal.authorities
+                    .map { obj: GrantedAuthority -> obj.authority }
+                context.claims.claim("roles", authorities)
+            }
+        }
+    }
+
+    // An instance of java.security.KeyPair with keys generated on startup used to create the JWKSource above.
+    private fun generateRsaKey(): KeyPair {
+        val keyPair: KeyPair = try {
+            val keyPairGenerator = KeyPairGenerator.getInstance("RSA")
+            keyPairGenerator.initialize(2048)
+            keyPairGenerator.generateKeyPair()
+        } catch (ex: Exception) {
+            throw IllegalStateException(ex)
+        }
+        return keyPair
+    }
+}
